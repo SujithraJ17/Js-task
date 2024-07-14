5f23c8899d6e081a9a914c4a9a482ae080cfbb2a
SujithraJ17 <sujithraprakash2005@gmail.com>
2024-07-13 19:45:24 -0700

    js Objects

---
+++ 03_JS_Objects/Ch3_pgm_01.js
@@ -0,0 +1,78 @@
+// Using variables to represent a book
+
+var bookTitle;
+var bookAuthor;
+
+bookTitle = "The Hobbit";
+bookAuthor = "J. R. R. Tolkien";
+
+console.log(bookTitle + " by " + bookAuthor);
+
+
+var bookTitle2 = "1984";
+var bookAuthor2 = "George Orwell";
+
+console.log(bookTitle2 + " by " + bookAuthor2);
+
+
+var bookTitle3 = "To Kill a Mockingbird";
+var bookAuthor3 = "Harper Lee";
+
+console.log(bookTitle3 + " by " + bookAuthor3);
+
+
+var bookTitle4 = "The Great Gatsby";
+var bookAuthor4 = "F. Scott Fitzgerald";
+
+console.log(bookTitle4 + " by " + bookAuthor4);
+
+
+var bookTitle5 = "Pride and Prejudice";
+var bookAuthor5 = "Jane Austen";
+
+console.log(bookTitle5 + " by " + bookAuthor5);
+
+
+var bookTitle6 = "Harry Potter and the Philosopher's Stone";
+var bookAuthor6 = "J. K. Rowling";
+
+console.log(bookTitle6 + " by " + bookAuthor6);
+
+
+var bookTitle7 = "The Catcher in the Rye";
+var bookAuthor7 = "J. D. Salinger";
+
+console.log(bookTitle7 + " by " + bookAuthor7);
+
+
+var bookTitle8 = "The Lord of the Rings";
+var bookAuthor8 = "J. R. R. Tolkien";
+
+console.log(bookTitle8 + " by " + bookAuthor8);
+
+
+var bookTitle9 = "Moby-Dick";
+var bookAuthor9 = "Herman Melville";
+
+console.log(bookTitle9 + " by " + bookAuthor9);
+
+
+var bookTitle10 = "Alice's Adventures in Wonderland";
+var bookAuthor10 = "Lewis Carroll";
+
+console.log(bookTitle10 + " by " + bookAuthor10);
+
+
+/* Further Adventures
+ *
+ * 1) Declare variables for a second book
+ *    and assign them values.
+ *
+ * 2) Add code to log its details to the console.
+ *
+ * 3) Repeat steps 1 and 2 for a third book.
+ *
+ * 4) Consider the code needed for 10 books.
+ *    For 100 books.
+ *
+ */
---
+++ 03_JS_Objects/Ch3_pgm_12.js
@@ -0,0 +1,35 @@
+// A blog post
+
+var post = {
+	id : 1,
+	title : "My Crazy Space Adventure",
+	author : "Philae",
+	created : "2015-06-21",
+	body : "You will not believe where I just woke up!! Only on a comet..."
+};
+
+console.log("Post Title:", post.title);
+console.log("Author:", post.author);
+console.log("Created Date:", post.created);
+
+
+var secondPost = {
+    id: 2,
+    title: "Exploring the Surface of Comet 67P",
+    author: "Rosetta",
+    created: "2015-07-10",
+    body: "Today we sent Philae to explore the comet's surface. Exciting times ahead!"
+};
+
+
+console.log("\nSecond Post Title:", secondPost.title);
+console.log("Author:", secondPost.author);
+console.log("Created Date:", secondPost.created);
+
+/* Further Adventures
+ *
+ * 1) Display some of the properties.
+ *
+ * 2) Add a second post.
+ *
+ */
