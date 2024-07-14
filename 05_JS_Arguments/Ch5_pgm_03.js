21ea68530c585c1cddaa7942371dd6489d781c8f
SujithraJ17 <sujithraprakash2005@gmail.com>
2024-07-13 19:58:27 -0700

    js aruguments

---
+++ 05_JS_Arguments/Ch5_pgm_03.js
@@ -0,0 +1,47 @@
+// Passing information to a function
+
+var showMessage;
+
+showMessage = function (message) {
+	console.log("The message is: " + message);
+};
+
+showMessage("It's full of stars!");
+
+showMessage("It's full of stars!"); 
+showMessage("Hello, world!");       
+showMessage("How are you today?");  
+
+showMessage("It's full of stars!");
+showMessage("Hello, world!");
+showMessage("How are you today?");
+
+var showMessage;
+
+showMessage = function (message) {
+    console.log("The message is: " + message + " - End of message");
+};
+
+showMessage("It's full of stars!");
+showMessage("Hello, world!");
+showMessage("How are you today?");
+
+
+
+/* Further Adventures
+ *
+ * 1) Change the message in the parentheses
+ *    when showMessage is called on line 9.
+ *
+ * The value in the parentheses when showMessage is called
+ * is an argument. "It's full of stars!" was the
+ * original argument on line 9.
+ *
+ * 2) Add two more calls to the showMessage function
+ *    with different arguments each time.
+ *
+ * 3) Change the showMessage function itself.
+ *    Make it add some extra text after the
+ *    message passed in.
+ *
+ */
---
+++ 05_JS_Arguments/Ch5_pgm_06.js
@@ -0,0 +1,85 @@
+// A function with two arguments
+
+var showSum;
+
+showSum = function (number1, number2) {
+	var total = number1 + number2;
+	console.log("The sum is " + total);
+};
+
+showSum(30, 23);
+showSum(2.8, -5);
+
+// A function with two arguments
+
+var showSum;
+
+// Function to show the sum of two numbers
+showSum = function (number1, number2) {
+    var total = number1 + number2;
+    console.log("The sum is " + total);
+};
+
+// Using the showSum function to add 56 and 74
+showSum(56, 74);
+
+// Using the showSum function to add 2.8 and -5
+showSum(2.8, -5);
+
+
+/* Further Adventures
+ *
+ * 1) Using multiplication:
+ */
+
+// Function to show the product of two numbers
+var showProduct = function (number1, number2) {
+    var product = number1 * number2;
+    console.log("The product is " + product);
+};
+
+// Using the showProduct function to multiply three pairs of numbers
+showProduct(3, 5);
+showProduct(10, -2);
+showProduct(7.5, 2);
+
+
+
+
+
+var showDifference = function (number1, number2) {
+    var difference = number1 - number2;
+    console.log("The difference is " + difference);
+};
+
+
+var showQuotient = function (number1, number2) {
+    var quotient = number1 / number2;
+    console.log("The quotient is " + quotient);
+};
+
+
+showDifference(10, 4);
+
+
+showQuotient(20, 5);
+
+
+/* Further Adventures
+ *
+ * 1) Use the showSum function to add 56 and 74.
+ *
+ * To multiply two numbers, use the * symbol.
+ * e.g. 3 * 5 is 3 multiplied by 5.
+ *
+ * 2) Create a showProduct function to multiply two numbers.
+ *
+ * 3) Use your function to multiply three pairs of numbers.
+ *
+ * To divide one number by another, use /.
+ * e.g. 10 / 2 is 10 divided by 2.
+ *
+ * 4) What about showDifference and showQuotient
+ *    for subtraction and division?
+ *
+ */
