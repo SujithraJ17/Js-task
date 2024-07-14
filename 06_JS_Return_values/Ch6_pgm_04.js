fdb9568adc01d3f8cab2501a00b35403dbfb7aa5
SujithraJ17 <sujithraprakash2005@gmail.com>
2024-07-13 20:00:05 -0700

    js return values

---
+++ 06_JS_Return_values/Ch6_pgm_04.js
@@ -0,0 +1,52 @@
+// Returning the sum of two numbers
+
+var add;
+
+add = function (number1, number2) {
+	var total = number1 + number2;
+
+	return total;
+};
+
+var sum = add(50, 23);
+
+console.log(sum);
+
+var sum = add(75, 42);
+console.log(sum);  
+
+console.log(`The sum of 50 and 23 is ${add(50, 23)}`);
+
+var sum = add(10, add(20, add(30, 40)));
+console.log(sum);  
+
+var addThree = function(number1, number2, number3) {
+    var total = number1 + number2 + number3;
+    return total;
+};
+
+var sum = addThree(10, 20, 30);
+console.log(sum);  
+
+
+
+
+
+/* Further Adventures
+ *
+ * 1) Find and display the sum of
+ *    a different pair of numbers
+ *
+ * 2) Change the call to console.log so that
+ *    the display on the console reads:
+ *    'The sum of 50 and 23 is 73'
+ *    using the add function to generate the answer.
+ *
+ * 3) Can you use the add function as it is
+ *    to add more than two numbers?
+ *    Hint: You can nest calls to add.
+ *
+ * 4) Create a function to return the sum of
+ *    three numbers given as arguments.
+ *
+ */
